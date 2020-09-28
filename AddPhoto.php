bfb2f62635b1b5513c945e2ecbcee29452165993
Prevail03 <71854258+Prevail03@users.noreply.github.com>
2020-09-28 07:43:05 +0300

    Update AddPhoto.php

--- AddPhoto.php
+++ AddPhoto.php
@@ -10,9 +10,9 @@
 		$location = "TeamPhotos/";
 		
 		if(move_uploaded_file($tmp_name,$location.$brandName.".jpg")){
-			$ImageURL =$location.$brandName.".jpg";
+			$ImageURL =$location.$brandName.".jpg";//to give name of the image in the folger and DB
 			$DB = new Database();
-			$brand = new Photo(0, $brandName, $ImageURL);
+			$brand = new Photo(0, $brandName, $ImageURL);//
 			$DB->addPhoto($brand);
 			header('Location: TeamDetails.php');
 		
